8bc865e674d962ee72788a15958877452edf7a07
yushare <582718643@qq.com>
2022-07-17 18:32:24 +0800

    feat: button

---
+++ packages/button/src/button.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="handleClick"><slot></slot></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ZzButton',
+  methods: {
+    handleClick (e) {
+      this.$emit('click', e)
+      e.preventDefault()
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
