30019fa4c931f7eba2790f71cedc53ec46624e85
yushare <582718643@qq.com>
2022-07-17 18:32:05 +0800

    feat: input

---
+++ packages/input/src/input.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="handleClick"><slot></slot></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ZzInput',
+  methods: {
+    handleClick (e) {
+      this.$emit('click', e)
+      e.preventDefault()
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
